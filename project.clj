48f7f281928b7cf6928e4eb3baaa85bc2db3ede2
Ryukzak Neskazov <ryukzak@mac-mini-admin.bn.local>
2010-11-30 15:09:00 +0300

    Wrapped in a project structure leinigen for easier use.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject pattern-match "1.0.1"
+  :description "Pattern matching per Haskell/ML"
+  :url "https://github.com/brool/clojure-misc"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
